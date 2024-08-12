13d6a7157a2661d96d8dc97fed5f4d6470c52afd
SHREYAO3 <anuradhasaha1900@gmail.com>
2024-08-12 16:37:31 +0530

    'updated'

---
+++ Week 2_Spring Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void findAllBooks() {
+        System.out.println("Displaying all books...");
+        // Logic to retrieve all books from the database
+    }
+}
